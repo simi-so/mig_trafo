db78922dc05d8894d2ebe01c60f51f34f88e66e2
Martin Schweizer <martin.schweizer@bd.so.ch>
2020-09-11 15:58:00 +0200

    Vorbereitungsscript für Arbeitsumgebung hinzugefuegt

---
+++ umgebung_vorbereiten.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dieses Script baut die Entwicklungsumgebung für die Simi-Datenmigration auf. 
+# Datenbankverbindungen müssen natürlich individuell angepasst werden. 
+#
+# Was ausserdem gemacht werden muss: 
+# - Der Benutzer postgresql muss sich ohne Passwort anmelden können
+# - Die "globals" müssen vorher in die DB importiert werden: 
+#   psql -h 10.36.43.200 -p 54323 -d config -U postgres -c "SET ROLE postgres" -f Dropbox/Arbeit/Simi/globals_geodb.rootso.org.dmp
+
+scp bjsvwsch@geoapp.verw.rootso.org:/opt/workspace/dbdump/soconfig_geodb.rootso.org.dmp Dropbox/Arbeit/Simi
+
+pg_restore -h 10.36.43.200 -p 54323 -U postgres -d config Dropbox/Arbeit/Simi/soconfig_geodb.rootso.org.dmp --clean
+
+psql -h 10.36.43.200 -p 54323 -d soconfig -U postgres -c "SET ROLE postgres" -q -f Dropbox/Arbeit/Simi/mig_trafo/mig/simi_ddl_20200910.sql
